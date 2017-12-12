34bd3ec1533ae0336a197a190b50959d3c14c534
yongzhen.ye <yongzhen.ye@boosal.com>
2017-12-12 18:23:27 +0800

    fix an issue

--- index.php
+++ index.php
@@ -1,3 +1,23 @@
+<?php
+header('Content-type:text/html;charset=utf-8');
+$base64_image_content = $_POST['imgBase64'];
+//匹配出图片的格式
+if (preg_match('/^(data:\s*image\/(\w+);base64,)/', $base64_image_content, $result)){
+$type = $result[2];
+$new_file = "upload/active/img/".date('Ymd',time())."/";
+if(!file_exists($new_file))
+{
+//检查是否有该文件夹，如果没有就创建，并给予最高权限
+mkdir($new_file, 0700);
+}
+$new_file = $new_file.time().".{$type}";
+if (file_put_contents($new_file, base64_decode(str_replace($result[1], '', $base64_image_content)))){
+echo '新文件保存成功：', $new_file;
+}else{
+echo '新文件保存失败';
+}
+}
+?>
 <!DOCTYPE html>
 <html lang="zh-CN">
 <head>
@@ -35,7 +55,18 @@
         //datapair = $sigdiv.jSignature("getData","base30");
         //$sigdiv.jSignature("setData", "data:" + datapair.join(","));
         $("#download").click(function(){
-            downloadFile("a.png", convertBase64UrlToBlob($("img").attr("src")));
+            var base64img = $("img").attr("src");
+            console.log(JSON.stringify(base64img));
+            var bytes=window.atob(urlData.split(',')[1]);
+
+            console.log(bytes);
+
+            $.ajax({
+                url:'index.php',
+                type:'post',
+                data:bytes
+            });
+//            downloadFile("a.png", convertBase64UrlToBlob(base64img));
         });
         $("#reset").click(function(){
             $sigdiv.jSignature("reset"); //重置画布，可以进行重新作画.
